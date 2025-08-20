0694a0d12a196cc929075b69969296a86bc7f0df
ashwindumane <ashwindumane0@gmail.com>
2025-08-20 12:36:23 +0530

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,13 @@ connectDB()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.json())
 app.use(cors({
-    origin:"*",
-    methods:["GET","POST","PUT","DELETE"],
-    allowedHeaders:["Content-Type", "Authorization"],
-    credentials: true 
+  origin:"*",
+  methods:["GET","POST","PUT","DELETE"],
+  allowedHeaders:["Content-Type", "Authorization"],
+  credentials: true 
 }))
+
 
 app.use('/api/auth', authRouter)
 app.use('/api/session', sessionRouter)
